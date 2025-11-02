ce92258e9f901725f24be624852c181d1ca2c2e0
Alexey Veretennikov <fourier@protonmail.ch>
2025-11-02 01:45:54 +0100

    Updated: add hypr key, volume up/down/mute

--- keyboards/keyboardio/atreus/keymaps/fourier/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/fourier/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,     KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    /*`*/             KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN ,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH ,
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_HYPR, KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH ,
   /*Su/Esc  Fn/Tab                               ctl      alt/-_            FN       (             )    Enter */
     SU_ESC, FN_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, ALT_QUO, KC_SPC,  MO(_FN), KC_LPRN, KC_RPRN,  SU_ENT),
 
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_FN] = LAYOUT( /* [> FN <] */
     KC_EXLM, KC_AT,   A_LEFT,  KC_DLR,  KC_PERC,                   KC_MINS, KC_PGUP, KC_UP,      KC_PGDN,   KC_PAST,
-    KC_LPRN, KC_MINS, A_RIGHT, KC_EQL,  KC_RPRN,                   KC_EQL,  KC_LEFT, KC_DOWN,    KC_RIGHT,  KC_PLUS,
+    KC_LPRN, KC_MINS, A_RIGHT, KC_EQL,  KC_RPRN,                   KC_EQL,  KC_LEFT, KC_DOWN,    KC_RIGHT,  KC_RCBR,
     KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR, C_LEFT,  KC_HOME, C_RIGHT,    KC_END,    KC_BSLS,
     QK_LLCK, KC_LGUI, KC_TRNS, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  MO(_SU), S(KC_MINS), S(KC_EQL), SU_ENT ),
 
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *        voldn  super shift bksp ctrl || alt space   Fn    [      ]   Enter
    */
   [_SU] = LAYOUT( /* [> SUPER <] */
-    KC_INS,  KC_7, KC_8, KC_9, KC_PSCR,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_4, KC_5, KC_6, KC_SCRL,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_1, KC_2, KC_3, KC_0,    _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
+    KC_INS,  KC_7, KC_8, KC_9, KC_PSCR,                   KC_VOLU,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
+    KC_DEL,  KC_4, KC_5, KC_6, KC_SCRL,                   KC_VOLD, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
+    KC_NO,   KC_1, KC_2, KC_3, KC_0,    _______, _______, KC_MUTE,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
     KC_NO,   KC_VOLD,    KC_LGUI,  KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_MINS, KC_EQL, KC_ENT )
 };
